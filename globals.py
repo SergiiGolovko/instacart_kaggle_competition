44670416e6130fd783a5de81d0158bf642526cbf
SergiiGolovko <sergii.golovko@gmail.com>
2017-07-22 07:42:40 -0400

    Add globals.

---
+++ globals.py
@@ -0,0 +1,11 @@
+# Define different run configurations.
+CONFIG_normal = {'CONFIG': 'config_normal',
+                 }
+
+CONFIG_test = {'CONFIG': 'config_test',
+               }
+
+# Select a configuration to run.
+CONFIG = CONFIG_normal
+# In order to run 'normal', just uncomment the following line, do NOT delete it
+CONFIG = CONFIG_test
