a30fc32e2cfd0c7202e7a995f6439de967aaa258
Sergii Golovko <sergii.golovko@gmail.com>
2017-07-22 11:32:02 -0400

    Modeling (#5)
    * Add globals.
    * Add modeling.
    * Fix a bug: now features are correctly created.

--- feature_engineering_test.py
+++ feature_engineering_test.py
@@ -7,37 +7,37 @@ from feature_engineering import features_by_product
 class FeaturesByProductTest(TestCase):
 
     def test_single_order(self):
-        cols = ['order_number', 'reordered']
-        data = DataFrame([[2, 0]], columns=cols)
+        cols = ['order_number', 'reordered', 'eval_set']
+        data = DataFrame([[2, 0, 0]], columns=cols)
         # label, total_orders, num_orders, num_reodered, last_time_ordered,
         # is_prev_order, is_before_prev_order]
-        exp_res = [0, 1, 0, 0, -1, 0, 0]
+        exp_res = [0, 1, 0, 0, -1, 0, 0, 0]
         res = features_by_product(data)
         self.assertEqual(exp_res, res)
 
     def test_two_consequitive_orders(self):
-        cols = ['order_number', 'reordered']
-        data = DataFrame([[2, 0], [3, 1]], columns=cols)
+        cols = ['order_number', 'reordered', 'eval_set']
+        data = DataFrame([[2, 0, 0], [3, 1, 0]], columns=cols)
         # label, total_orders, num_orders, last_time_ordered, is_prev_order,
         # is_before_prev_order]
-        exp_res = [1, 2, 1, 0, 2, 1, 0]
+        exp_res = [1, 2, 1, 0, 2, 1, 0, 0]
         res = features_by_product(data)
         self.assertEqual(exp_res, res)
 
     def test_two_nonconsequitive_orders(self):
-        cols = ['order_number', 'reordered']
-        data = DataFrame([[1, 0], [3, 0]], columns=cols)
+        cols = ['order_number', 'reordered', 'eval_set']
+        data = DataFrame([[1, 0, 0], [3, 0, 0]], columns=cols)
         # label, total_orders, num_orders, last_time_ordered, is_prev_order,
         # is_before_prev_order]
-        exp_res = [0, 2, 1, 0, 1, 0, 1]
+        exp_res = [0, 2, 1, 0, 1, 0, 1, 0]
         res = features_by_product(data)
         self.assertEqual(exp_res, res)
 
     def test_three_consequitive_orders(self):
-        cols = ['order_number', 'reordered']
-        data = DataFrame([[1, 0], [2, 1], [3, 1]], columns=cols)
+        cols = ['order_number', 'reordered', 'eval_set']
+        data = DataFrame([[1, 0, 0], [2, 1, 0], [3, 1, 0]], columns=cols)
         # label, total_orders, num_orders, last_time_ordered, is_prev_order,
         # is_before_prev_order
-        exp_res = [1, 2, 2, 1, 2, 1, 1]
+        exp_res = [1, 2, 2, 1, 2, 1, 1, 0]
         res = features_by_product(data)
         self.assertEqual(exp_res, res)
