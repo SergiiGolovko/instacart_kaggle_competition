e57b94de3c15f21fabb3b4dc4514d55b81627f81
SergiiGolovko <sergii.golovko@gmail.com>
2017-07-22 22:46:54 -0400

    Add evaluate model performane.

--- generate_predictions.py
+++ generate_predictions.py
@@ -1,6 +1,8 @@
-from logging import info
+from logging import basicConfig, info, debug, DEBUG
 from numpy import array, concatenate, zeros
 from pandas import DataFrame
+
+from pickle_utils import try_load
 
 
 def mean_F1_score(y_true, y_pred_proba, user_id, threshold=0.5):
@@ -9,7 +11,7 @@ def mean_F1_score(y_true, y_pred_proba, user_id, threshold=0.5):
 
     y_true = array(y_true).reshape((len(y_true), 1))
     y_pred_proba = array(y_pred_proba).reshape((len(y_pred_proba), 1))
-    user_id = array(user_id).reshape((len(y_pred_proba), 1))
+    user_id = array(user_id).reshape((len(user_id), 1))
 
     y_pred = (y_pred_proba > threshold).astype(int)
     cols = ['user_id', 'y_true', 'y_pred']
@@ -34,6 +36,33 @@ def mean_F1_score(y_true, y_pred_proba, user_id, threshold=0.5):
         lambda x: 2 * (x.precision * x.recall) / (x.precision + x.recall)
         if x.precision + x.recall > 0 else 0,
         axis=1)
-    info('Mean F1 score is created.')
+    info('Mean F1 score is calculated.')
 
     return res_df['F1'].mean()
+
+
+def evaluate_predictions(pred_file, features_file):
+
+    info('Evaluating predictions.')
+
+    info('Loading data.')
+    y_pred_proba = try_load(pred_file, raise_error=True)
+    wanted_cols = ['user_id', 'label', 'train_set']
+    df = try_load(features_file, raise_error=True)[wanted_cols]
+    df = df[df['train_set'] == 1]
+    debug('Data size: %d, unique users_id: %d'
+          % (len(df), df['user_id'].nunique()))
+    info('Finished loading data.')
+    debug('Scoring predictions')
+    score = mean_F1_score(df['label'], y_pred_proba, df['user_id'], 0.05)
+    debug('Score is: %.5f' % score)
+    info('Finished evaluating predictions.')
+
+
+if __name__ == '__main__':
+
+    format = '%(asctime)s %(levelname)s %(filename)s %(funcName)s %(message)s'
+    basicConfig(level=DEBUG, format=format, datefmt='%m/%d/%Y %I:%M:%S')
+    features_file = './pickles/features_1_3223.pckl'
+    pred_file = './pickles/metafeatures/XGBClassifier'
+    evaluate_predictions(pred_file, features_file)
